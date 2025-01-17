82383ccd99be6c82d32d731eb9582c62939e5230
PradoUrqJose <josepu03@gmail.com>
2025-01-17 13:09:30 -0500

    Instalation of the Notifly library and configuration in the Turnos Component, for the elimination confirm modal, then created the configuration notifly file in the src/app/config, and finally a font  was installed for the notifly configuration (QuickSand)

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,3 +1,4 @@
+import { configurarNotiflix } from './app/config/notiflix-config';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { bootstrapApplication } from '@angular/platform-browser';
@@ -5,6 +6,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import './styles.css';
 
+configurarNotiflix();
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
