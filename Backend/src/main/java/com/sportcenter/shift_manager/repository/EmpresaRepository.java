cf1cf0ceb7558f180707a9cb79c37ab9009bdfac
Jose Prado <josepu03@gmail.com>
2025-01-26 18:41:59 -0500

    New function of BuscarEmpresa por estado in EmpresaRepository

--- Backend/src/main/java/com/sportcenter/shift_manager/repository/EmpresaRepository.java
+++ Backend/src/main/java/com/sportcenter/shift_manager/repository/EmpresaRepository.java
@@ -3,9 +3,11 @@ package com.sportcenter.shift_manager.repository;
 import com.sportcenter.shift_manager.model.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
     Optional<Empresa> findByNombre(String nombre);
     Optional<Empresa> findByRuc(String ruc); // Nuevo método para buscar por RUC
+    List<Empresa> findByHabilitada(boolean habilitada);
 }
