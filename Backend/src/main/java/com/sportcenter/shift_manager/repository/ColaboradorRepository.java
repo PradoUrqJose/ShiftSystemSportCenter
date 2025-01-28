f694de6ff2d1ebea0ada400041836bf0dc2fa054
Jose Prado <josepu03@gmail.com>
2025-01-27 22:04:45 -0500

    Adding the findByHabilitado function in Colaborador Repository

--- Backend/src/main/java/com/sportcenter/shift_manager/repository/ColaboradorRepository.java
+++ Backend/src/main/java/com/sportcenter/shift_manager/repository/ColaboradorRepository.java
@@ -1,6 +1,7 @@
 package com.sportcenter.shift_manager.repository;
 
 import com.sportcenter.shift_manager.model.Colaborador;
+import com.sportcenter.shift_manager.model.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface ColaboradorRepository extends JpaRepository<Colaborador, Long> {
     List<Colaborador> findByEmpresaId(Long empresaId);
+
+    List<Colaborador> findByHabilitado(boolean habilitado);
+
 }
