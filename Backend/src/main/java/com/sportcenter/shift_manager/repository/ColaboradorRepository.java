c7eb9718985f8eebacfb9aeb7df63fadb3433e6f
Jose Prado <josepu03@gmail.com>
2025-02-07 22:26:19 -0500

    feat: add methods to find Colaborador by email, dni, and name and surname
    - Add findByEmail, findByDni, and findByNombreAndApellido methods to ColaboradorRepository.

--- Backend/src/main/java/com/sportcenter/shift_manager/repository/ColaboradorRepository.java
+++ Backend/src/main/java/com/sportcenter/shift_manager/repository/ColaboradorRepository.java
@@ -6,11 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ColaboradorRepository extends JpaRepository<Colaborador, Long> {
     List<Colaborador> findByEmpresaId(Long empresaId);
 
     List<Colaborador> findByHabilitado(boolean habilitado);
-
+    Optional<Colaborador> findByEmail(String email);
+    Optional<Colaborador> findByDni(String dni);
+    Optional<Colaborador> findByNombreAndApellido(String nombre, String apellido);
 }
