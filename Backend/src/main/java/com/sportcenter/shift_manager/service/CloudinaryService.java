adc1f056dbcfd030f711f26c02fb6db436ce4304
Jose Prado <josepu03@gmail.com>
2025-02-08 09:29:22 -0500

    feat: Create the cloudinary Service for the Cloudinary Upload and Delete Image

---
+++ Backend/src/main/java/com/sportcenter/shift_manager/service/CloudinaryService.java
@@ -0,0 +1,37 @@
+package com.sportcenter.shift_manager.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService(
+            @Value("${cloudinary.cloud-name}") String cloudName,
+            @Value("${cloudinary.api-key}") String apiKey,
+            @Value("${cloudinary.api-secret}") String apiSecret) {
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        ));
+    }
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+        return uploadResult.get("secure_url").toString(); // Retorna la URL pública
+    }
+
+    // Método para eliminar una imagen en Cloudinary
+    public String deleteImage(String publicId) throws IOException {
+        Map result = cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+        return result.get("result").toString(); // Retorna "ok" si la imagen fue eliminada
+    }
+}
