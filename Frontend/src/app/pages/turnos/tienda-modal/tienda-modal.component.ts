8f9215ddac7199a19982ba15eb6a5467b8b843a7
Jose Prado <josepu03@gmail.com>
2025-02-18 21:37:31 -0500

    feat: Create a Tienda Modal for Turnos Component

---
+++ Frontend/src/app/pages/turnos/tienda-modal/tienda-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tienda-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './tienda-modal.component.html',
+  styleUrl: './tienda-modal.component.css'
+})
+export class TiendaModalComponent {
+
+}
