0a6fa87cebb54d35c1fef1d3e3f21b83812207d3
Jose Prado <josepu03@gmail.com>
2025-02-18 21:37:07 -0500

    feat: Create a modal component of Turnos Component

---
+++ Frontend/src/app/pages/turnos/turno-modal/turno-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-turno-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './turno-modal.component.html',
+  styleUrl: './turno-modal.component.css'
+})
+export class TurnoModalComponent {
+
+}
