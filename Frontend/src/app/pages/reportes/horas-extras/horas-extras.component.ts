76446a76efaaea79ab30de0bed9fa425ee33477b
Jose Prado <josepu03@gmail.com>
2025-02-19 23:32:50 -0500

    REPORTS - Creando Componente Reporte Horas Extras HIJO

---
+++ Frontend/src/app/pages/reportes/horas-extras/horas-extras.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horas-extras',
+  standalone: true,
+  imports: [],
+  templateUrl: './horas-extras.component.html',
+  styleUrl: './horas-extras.component.css'
+})
+export class HorasExtrasComponent {
+
+}
