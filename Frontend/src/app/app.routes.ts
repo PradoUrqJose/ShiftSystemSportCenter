01270a3a84899ade78652a4258706b85b39a7fbf
Jose Prado <josepu03@gmail.com>
2025-02-19 23:47:44 -0500

    ** Agregando rutas a Reportes Padre y subenlaces a Reportes Hijos desde routes.ts

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,11 +3,24 @@ import { Routes } from '@angular/router';
 import ColaboradoresComponent from './pages/colaboradores/colaboradores.component';
 import EmpresasComponent from './pages/empresas/empresas.component';
 import TurnosComponent from './pages/turnos/turnos.component';
+import ReportesComponent from './pages/reportes/reportes.component';
+import { HorasTrabajadasComponent } from './pages/reportes/horas-trabajadas/horas-trabajadas.component';
+import { TurnosFeriadosComponent } from './pages/reportes/turnos-feriados/turnos-feriados.component';
+import { HorasExtrasComponent } from './pages/reportes/horas-extras/horas-extras.component';
 
 export const routes: Routes = [
   { path: 'empresas', component: EmpresasComponent },
   { path: 'colaboradores', component: ColaboradoresComponent },
   { path: 'turnos', component: TurnosComponent },
-  { path: '', redirectTo: 'turnos', pathMatch: 'full' },
+  {
+    path: 'reportes',
+    component: ReportesComponent,
+    children: [
+      { path: 'horas-trabajadas', component: HorasTrabajadasComponent },
+      { path: 'turnos-feriados', component: TurnosFeriadosComponent },
+      { path: 'horas-extras', component: HorasExtrasComponent },
+      { path: '', redirectTo: 'horas-trabajadas', pathMatch: 'full' }
+    ]
+  }, { path: '', redirectTo: 'turnos', pathMatch: 'full' },
   { path: '**', redirectTo: 'empresas' }
 ];
