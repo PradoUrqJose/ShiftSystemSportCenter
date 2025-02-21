93aab472bf6db4359906d153e0c276cb929c30fb
Jose Prado <josepu03@gmail.com>
2025-02-21 01:14:04 -0500

    Modificando Feriados de Lista estatica a lista dinamica SQL

--- Backend/src/main/java/com/sportcenter/shift_manager/service/FeriadoService.java
+++ Backend/src/main/java/com/sportcenter/shift_manager/service/FeriadoService.java
@@ -1,42 +1,51 @@
 package com.sportcenter.shift_manager.service;
 
 import com.sportcenter.shift_manager.model.Feriado;
+import com.sportcenter.shift_manager.repository.FeriadoRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class FeriadoService {
+    private final FeriadoRepository feriadoRepository;
 
-    // Lista fija de feriados
-    private final List<Feriado> feriados = Arrays.asList(
-            new Feriado(LocalDate.of(2025, 1, 1), "Año Nuevo"),
-            new Feriado(LocalDate.of(2025, 4, 17), "Jueves Santo"),
-            new Feriado(LocalDate.of(2025, 4, 18), "Viernes Santo"),
-            new Feriado(LocalDate.of(2025, 5, 1), "Día del Trabajo"),
-            new Feriado(LocalDate.of(2025, 6, 7), "Batalla de Arica y Día de la Bandera"),
-            new Feriado(LocalDate.of(2025, 6, 29), "Día de San Pedro y San Pablo"),
-            new Feriado(LocalDate.of(2025, 7, 23), "Día de la Fuerza Aérea del Perú"),
-            new Feriado(LocalDate.of(2025, 7, 28), "Fiestas Patrias"),
-            new Feriado(LocalDate.of(2025, 7, 29), "Fiestas Patrias"),
-            new Feriado(LocalDate.of(2025, 8, 6), "Batalla de Junín"),
-            new Feriado(LocalDate.of(2025, 8, 30), "Santa Rosa de Lima"),
-            new Feriado(LocalDate.of(2025, 10, 8), "Combate de Angamos"),
-            new Feriado(LocalDate.of(2025, 11, 1), "Día de Todos los Santos"),
-            new Feriado(LocalDate.of(2025, 12, 8), "Inmaculada Concepción"),
-            new Feriado(LocalDate.of(2025, 12, 9), "Batalla de Ayacucho"),
-            new Feriado(LocalDate.of(2025, 12, 25), "Navidad")
-    );
-
-    // Método para obtener todos los feriados
-    public List<Feriado> getAllFeriados() {
-        return feriados;
+    public FeriadoService(FeriadoRepository feriadoRepository) {
+        this.feriadoRepository = feriadoRepository;
     }
 
-    // Método para verificar si una fecha es feriado
+    @PostConstruct
+    public void initializeFeriados() {
+        if (feriadoRepository.count() == 0) {
+            List<Feriado> feriadosIniciales = Arrays.asList(
+                    new Feriado(null, LocalDate.of(2025, 1, 1), "Año Nuevo"),
+                    new Feriado(null, LocalDate.of(2025, 4, 17), "Jueves Santo"),
+                    new Feriado(null, LocalDate.of(2025, 4, 18), "Viernes Santo"),
+                    new Feriado(null, LocalDate.of(2025, 5, 1), "Día del Trabajo"),
+                    new Feriado(null, LocalDate.of(2025, 6, 7), "Batalla de Arica y Día de la Bandera"),
+                    new Feriado(null, LocalDate.of(2025, 6, 29), "Día de San Pedro y San Pablo"),
+                    new Feriado(null, LocalDate.of(2025, 7, 23), "Día de la Fuerza Aérea del Perú"),
+                    new Feriado(null, LocalDate.of(2025, 7, 28), "Fiestas Patrias"),
+                    new Feriado(null, LocalDate.of(2025, 7, 29), "Fiestas Patrias"),
+                    new Feriado(null, LocalDate.of(2025, 8, 6), "Batalla de Junín"),
+                    new Feriado(null, LocalDate.of(2025, 8, 30), "Santa Rosa de Lima"),
+                    new Feriado(null, LocalDate.of(2025, 10, 8), "Combate de Angamos"),
+                    new Feriado(null, LocalDate.of(2025, 11, 1), "Día de Todos los Santos"),
+                    new Feriado(null, LocalDate.of(2025, 12, 8), "Inmaculada Concepción"),
+                    new Feriado(null, LocalDate.of(2025, 12, 9), "Batalla de Ayacucho"),
+                    new Feriado(null, LocalDate.of(2025, 12, 25), "Navidad")
+            );
+            feriadoRepository.saveAll(feriadosIniciales);
+        }
+    }
+
+    public List<Feriado> getAllFeriados() {
+        return feriadoRepository.findAll();
+    }
+
     public boolean isFeriado(LocalDate fecha) {
-        return feriados.stream().anyMatch(feriado -> feriado.getFecha().equals(fecha));
+        return feriadoRepository.existsByFecha(fecha);
     }
 }
