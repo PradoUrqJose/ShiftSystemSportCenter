0a765390a87bc56249790df1d7a41260b54b6074
Jose Prado <josepu03@gmail.com>
2025-02-21 01:13:26 -0500

    Agregando Entity lombok

--- Backend/src/main/java/com/sportcenter/shift_manager/model/Feriado.java
+++ Backend/src/main/java/com/sportcenter/shift_manager/model/Feriado.java
@@ -1,15 +1,23 @@
 package com.sportcenter.shift_manager.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Feriado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
     private LocalDate fecha;
+
     private String descripcion;
 }
