a01fc928e37bf16be51213ed2b21c5c9cf46c7b7
Jose Prado <josepu03@gmail.com>
2025-02-23 23:57:05 -0500

    feat: Crear nuevo servicio de puesto en frontend

---
+++ Frontend/src/app/services/puesto.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
+export interface Puesto {
+  id?: number;
+  nombre: string;
+  descripcion?: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PuestoService {
+  private apiUrl = 'http://localhost:8080/api/puestos';
+
+  constructor(private http: HttpClient) {}
+
+  getPuestos(): Observable<Puesto[]> {
+    return this.http.get<Puesto[]>(this.apiUrl);
+  }
+
+  getPuestoById(id: number): Observable<Puesto> {
+    return this.http.get<Puesto>(`${this.apiUrl}/${id}`).pipe(
+      map(puesto => ({ ...puesto, id: Number(puesto.id) }))
+    );
+  }
+
+  addPuesto(puesto: Puesto): Observable<Puesto> {
+    return this.http.post<Puesto>(this.apiUrl, puesto);
+  }
+
+  updatePuesto(id: number, puesto: Puesto): Observable<Puesto> {
+    return this.http.put<Puesto>(`${this.apiUrl}/${id}`, puesto);
+  }
+
+  deletePuesto(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
