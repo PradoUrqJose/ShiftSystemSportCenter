259ab84061ab2732ec69c54fc43f66b43eb69d0b
Jose Prado <josepu03@gmail.com>
2025-03-03 21:04:19 -0500

    Tiendas Backend para Producción

--- Backend/src/main/java/com/sportcenter/shift_manager/repository/TiendaRepository.java
+++ Backend/src/main/java/com/sportcenter/shift_manager/repository/TiendaRepository.java
@@ -4,6 +4,9 @@ import com.sportcenter.shift_manager.model.Tienda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TiendaRepository extends JpaRepository<Tienda, Long> {
+    Optional<Tienda> findByNombre(String nombre); // Opcional, para validar duplicados
 }
