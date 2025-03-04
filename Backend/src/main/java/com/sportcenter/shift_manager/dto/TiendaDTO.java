39a63af5a0d7e17cad6bf73d847efea14ecd78c9
Jose Prado <josepu03@gmail.com>
2025-03-03 21:04:36 -0500

    Último cambio para Tienda Backend

---
+++ Backend/src/main/java/com/sportcenter/shift_manager/dto/TiendaDTO.java
@@ -0,0 +1,21 @@
+package com.sportcenter.shift_manager.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TiendaDTO {
+    private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(max = 100, message = "El nombre no puede exceder 100 caracteres")
+    private String nombre;
+
+    @Size(max = 255, message = "La dirección no puede exceder 255 caracteres")
+    private String direccion;
+}
