588716e98d76d7bdba893331027eb947bc0725e6
Jose Prado <josepu03@gmail.com>
2025-03-06 20:19:50 -0500

    Cambiando el backend a Render permanentemente

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://shift-manager-fly-thrumming-dawn-7994.fly.dev/api', // Reemplaza con tu dominio real
+  apiUrl: 'https://shift-manager-305l.onrender.com/api', // Reemplaza con tu dominio real
+  // apiUrl: 'https://shift-manager-fly-thrumming-dawn-7994.fly.dev/api', // Reemplaza con tu dominio real
 };
